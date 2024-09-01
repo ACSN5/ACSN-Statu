3ba15f620d322636e932c9e51a9eae1d09beeca3
[ACSN]Beluga <1447754529@qq.com>
2024-09-01 16:55:49 +0800

    Add files via upload

---
+++ config.js
@@ -0,0 +1,29 @@
+window.Config = {
+
+  // 显示标题
+  SiteName: '创活网网站服务状态',
+
+  // UptimeRobot Api Keys
+  // 支持 Monitor-Specific 和 Read-Only
+  ApiKeys: [
+    'u2665648-82d38f63636735c61f4d6410',
+  ],
+
+  // 日志天数
+  CountDays: 60,
+
+  // 是否显示检测站点的链接
+  ShowLink: true,
+
+  // 导航栏菜单
+  Navi: [
+    {
+      text: '主页',
+      url: 'https://acsn.cn/'
+    },
+    {
+      text: 'Blog',
+      url: 'https://blog.acsn.cn/'
+    },
+  ],
+};
